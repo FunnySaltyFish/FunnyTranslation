e3b829b156089c8784a02d65cac85e418661cdd1
FunnySaltyFish <3036548996@qq.com>
2020-01-01 13:43:50 +0800

    Initial commit

---
+++ app/src/main/java/com/funny/translation/MainActivity.java
@@ -0,0 +1,14 @@
+package com.funny.translation;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
