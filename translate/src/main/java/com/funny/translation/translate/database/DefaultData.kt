1277fc35f4fab7437db27ebad7f53699e01f710d
FunnySaltyFish <3036548996@qq.com>
2021-10-12 23:52:42 +0800

    第一个版本可以用了[20211012]
    Former-commit-id: 94345360650b5ab27dff456dbed5c7ae965f2154

--- translate/src/main/java/com/funny/translation/translate/database/DefaultData.kt
+++ translate/src/main/java/com/funny/translation/translate/database/DefaultData.kt
@@ -7,6 +7,7 @@ import com.funny.translation.js.bean.JsBean
 import com.funny.translation.translate.FunnyApplication
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import kotlin.properties.Delegates
 
@@ -14,20 +15,24 @@ object DefaultData {
     private val TAG = "DefaultData"
     suspend fun getDefaultJsList(scope: CoroutineScope): List<JsBean> {
         var arr by Delegates.notNull<List<JsBean>>()
-        withContext(Dispatchers.IO) {
-            val fileNames = arrayOf("谷歌翻译实现.js")
-            arr = fileNames.map {
-                val code = FunnyApplication.ctx.readAssets(it)
-                val jsBean = JsBean(code = code)
-                val jsEngine = JsEngine(jsBean)
-                jsEngine.loadBasicConfigurations({
-
-                }, { e ->
-                    Log.d(TAG, "loadDefaultData Failed!: ${e.message}")
-                })
-                jsEngine.jsBean
+        Log.d(TAG, "getDefaultJsList: 开始执行")
+        scope.launch {
+            withContext(Dispatchers.IO) {
+                val fileNames = arrayOf("谷歌翻译实现.js")
+                arr = fileNames.map {
+                    val code = FunnyApplication.ctx.readAssets(it)
+                    val jsBean = JsBean(code = code)
+                    val jsEngine = JsEngine(jsBean)
+                    jsEngine.loadBasicConfigurations({
+                        Log.d(TAG, "getDefaultJsList: 加载js引擎完毕")
+                    }, { e ->
+                        Log.d(TAG, "loadDefaultData Failed!: ${e.message}")
+                    })
+                    jsEngine.jsBean
+                }
             }
-        }
+        }.join()
+        Log.d(TAG, "getDefaultJsList: 准备返回js引擎列表")
         return arr
     }
 }
