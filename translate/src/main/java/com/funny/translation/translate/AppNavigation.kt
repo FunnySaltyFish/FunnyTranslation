528748e7ce96c1389b18c455e8f5ce579a01f523
FunnySaltyFish <3036548996@qq.com>
2022-06-03 17:52:33 +0800

    v2.3.1 release
    Former-commit-id: aefd975d755f2b82e04856d44d5d4eac265ab450
    Former-commit-id: 279d5cdad94764f0cc8db094c3c9eb4b8bd651ce

--- translate/src/main/java/com/funny/translation/translate/AppNavigation.kt
+++ translate/src/main/java/com/funny/translation/translate/AppNavigation.kt
@@ -5,15 +5,12 @@ import androidx.activity.compose.BackHandler
 import androidx.compose.animation.AnimatedContentScope
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
@@ -35,11 +32,6 @@ import com.funny.translation.translate.ui.settings.SortResult
 import com.funny.translation.translate.ui.thanks.ThanksScreen
 import com.funny.translation.translate.ui.theme.TransTheme
 import com.funny.translation.translate.ui.widget.CustomNavigation
-import com.funny.translation.translate.ui.widget.SimpleDialog
-import com.google.accompanist.insets.LocalWindowInsets
-import com.google.accompanist.insets.ProvideWindowInsets
-import com.google.accompanist.insets.rememberInsetsPaddingValues
-import com.google.accompanist.insets.statusBarsPadding
 import com.google.accompanist.navigation.animation.AnimatedNavHost
 import com.google.accompanist.navigation.animation.composable
 import com.google.accompanist.navigation.animation.navigation
@@ -101,139 +93,155 @@ fun AppNavigation(
         LocalSnackbarState provides scaffoldState.snackbarHostState,
         LocalDataSaver provides DataSaverUtils
     ) {
-        ProvideWindowInsets {
-            val bottomBarContentPadding = rememberInsetsPaddingValues(
-                LocalWindowInsets.current.navigationBars
-            )
-            TransTheme {
-                val systemUiController = rememberSystemUiController()
-                val useDarkIcons = MaterialTheme.colors.isLight
-                val navigationBarColor = MaterialTheme.colors.background.copy(alpha = 0.95f)
-                LaunchedEffect(key1 = systemUiController) {
-                    systemUiController.setSystemBarsColor(Color.Transparent, darkIcons = useDarkIcons)
-                    systemUiController.setNavigationBarColor(
-                        if (!useDarkIcons) Color.Transparent else navigationBarColor, darkIcons = useDarkIcons
-                    )
+        TransTheme {
+            val systemUiController = rememberSystemUiController()
+            val useDarkIcons = MaterialTheme.colors.isLight
+            val navigationBarColor = MaterialTheme.colors.background.copy(alpha = 0.95f)
+            LaunchedEffect(key1 = systemUiController) {
+                systemUiController.setSystemBarsColor(Color.Transparent, darkIcons = useDarkIcons)
+                systemUiController.setNavigationBarColor(
+                    if (!useDarkIcons) Color.Transparent else navigationBarColor,
+                    darkIcons = useDarkIcons
+                )
 
-                    systemUiController.isNavigationBarVisible = !DataSaverUtils.readData(Consts.KEY_HIDE_NAVIGATION_BAR,false)
-                    systemUiController.isStatusBarVisible = !DataSaverUtils.readData(Consts.KEY_HIDE_STATUS_BAR,true)
-                }
-                Scaffold(
-                    bottomBar = {
-                        val currentScreen = navController.currentScreenAsState()
-                        CustomNavigation(
-                            backgroundColor = navigationBarColor,
-                            contentPadding = bottomBarContentPadding,
-                            screens = arrayOf(
-                                TranslateScreen.MainScreen,
-                                TranslateScreen.PluginScreen,
-                                TranslateScreen.SettingScreen,
-                                TranslateScreen.ThanksScreen
-                            ),
-                            currentScreen = currentScreen.value
-                        ) { screen ->
-                            if (screen == currentScreen) return@CustomNavigation
+                systemUiController.isNavigationBarVisible =
+                    !DataSaverUtils.readData(Consts.KEY_HIDE_NAVIGATION_BAR, false)
+                systemUiController.isStatusBarVisible =
+                    !DataSaverUtils.readData(Consts.KEY_HIDE_STATUS_BAR, true)
+            }
+            Scaffold(
+                bottomBar = {
+                    val currentScreen = navController.currentScreenAsState()
+                    CustomNavigation(
+                        backgroundColor = navigationBarColor,
+                        contentPadding = WindowInsets.navigationBars.asPaddingValues(),
+                        screens = arrayOf(
+                            TranslateScreen.MainScreen,
+                            TranslateScreen.PluginScreen,
+                            TranslateScreen.SettingScreen,
+                            TranslateScreen.ThanksScreen
+                        ),
+                        currentScreen = currentScreen.value
+                    ) { screen ->
+                        if (screen == currentScreen) return@CustomNavigation
 
-                            val currentRoute = navBackStackEntry?.destination?.route
-                            Log.d(TAG, "AppNavigation: $currentRoute")
+                        val currentRoute = navBackStackEntry?.destination?.route
+                        Log.d(TAG, "AppNavigation: $currentRoute")
 
-                            //currentScreen = screen
-                            navController.navigate(screen.route) {
-                                //当底部导航导航到在非首页的页面时，执行手机的返回键 回到首页
-                                popUpTo(navController.graph.startDestinationId) {
-                                    saveState = true
-                                    //currentScreen = TranslateScreen.MainScreen
-                                }
-                                //从名字就能看出来 跟activity的启动模式中的SingleTop模式一样 避免在栈顶创建多个实例
-                                launchSingleTop = true
-                                //切换状态的时候保存页面状态
-                                restoreState = true
+                        //currentScreen = screen
+                        navController.navigate(screen.route) {
+                            //当底部导航导航到在非首页的页面时，执行手机的返回键 回到首页
+                            popUpTo(navController.graph.startDestinationId) {
+                                saveState = true
+                                //currentScreen = TranslateScreen.MainScreen
                             }
-                        }
-                    },
-                    scaffoldState = scaffoldState
-                ) {
-                    AnimatedNavHost(
-                        navController = navController,
-                        startDestination = TranslateScreen.MainScreen.route,
-                        modifier = Modifier
-                            .statusBarsPadding()
-                            // avoid content being sheltered
-                            .padding(bottom = it.calculateBottomPadding())
-                    ) {
-                        composable(TranslateScreen.MainScreen.route, deepLinks = listOf(
-                            navDeepLink { uriPattern = "funny://translation/translate?text={text}&sourceId={sourceId}&targetId={targetId}" }
-                        )) {
-                            MainScreen()
-                        }
-                        val animDuration = 700
-                        navigation(
-                            startDestination = TranslateScreen.SettingScreen.route,
-                            route = "nav_composition_setting",
-                            enterTransition = {
-                                slideIntoContainer(AnimatedContentScope.SlideDirection.Left, animationSpec = tween(animDuration))
-                            },
-                            exitTransition = {
-                                slideOutOfContainer(AnimatedContentScope.SlideDirection.Left, animationSpec = tween(animDuration))
-                            },
-                            popEnterTransition = {
-                                slideIntoContainer(AnimatedContentScope.SlideDirection.Right, animationSpec = tween(animDuration))
-                            },
-                            popExitTransition = {
-                                slideOutOfContainer(AnimatedContentScope.SlideDirection.Right, animationSpec = tween(animDuration))
-                            }
-                        ) {
-                            composable(TranslateScreen.SettingScreen.route) {
-                                SettingsScreen()
-                            }
-                            composable(
-                                TranslateScreen.AboutScreen.route,
-                            ) {
-                                AboutScreen()
-                            }
-                            composable(
-                                TranslateScreen.SortResultScreen.route,
-                            ) {
-                                SortResult(Modifier.fillMaxSize())
-                            }
-                            composable(
-                                TranslateScreen.SelectLanguageScreen.route
-                            ){
-                                SelectLanguage(modifier = Modifier.fillMaxSize())
-                            }
-                        }
-
-                        composable(TranslateScreen.PluginScreen.route) {
-                            PluginScreen()
-                        }
-                        composable(TranslateScreen.ThanksScreen.route) {
-                            ThanksScreen()
+                            //从名字就能看出来 跟activity的启动模式中的SingleTop模式一样 避免在栈顶创建多个实例
+                            launchSingleTop = true
+                            //切换状态的时候保存页面状态
+                            restoreState = true
                         }
                     }
-                }
-
-                var firstOpenApplication by rememberDataSaverState(key = Consts.KEY_FIRST_OPEN_APP, default = true)
-                if (firstOpenApplication){
-                    AlertDialog(
-                        onDismissRequest = {  },
-                        text = {
-                            MarkdownText(markdown = "请认真阅读并同意[隐私政策](https://api.funnysaltyfish.fun/trans/v1/api/privacy)后，方可使用本应用")
-                        },
-                        confirmButton = {
-                            Button(onClick = { firstOpenApplication = false }) {
-                                Text(stringResource(R.string.agree))
-                            }
-                        },
-                        dismissButton = {
-                            Button(onClick = exitAppAction) {
-                                Text(stringResource(R.string.not_agree))
-                            }
+                },
+                scaffoldState = scaffoldState
+            ) {
+                AnimatedNavHost(
+                    navController = navController,
+                    startDestination = TranslateScreen.MainScreen.route,
+                    modifier = Modifier.statusBarsPadding()
+                        // avoid content being sheltered
+                        .padding(bottom = it.calculateBottomPadding())
+                ) {
+                    composable(TranslateScreen.MainScreen.route, deepLinks = listOf(
+                        navDeepLink {
+                            uriPattern =
+                                "funny://translation/translate?text={text}&sourceId={sourceId}&targetId={targetId}"
                         }
-                    )
+                    )) {
+                        MainScreen()
+                    }
+                    val animDuration = 700
+                    navigation(
+                        startDestination = TranslateScreen.SettingScreen.route,
+                        route = "nav_composition_setting",
+                        enterTransition = {
+                            slideIntoContainer(
+                                AnimatedContentScope.SlideDirection.Left,
+                                animationSpec = tween(animDuration)
+                            )
+                        },
+                        exitTransition = {
+                            slideOutOfContainer(
+                                AnimatedContentScope.SlideDirection.Left,
+                                animationSpec = tween(animDuration)
+                            )
+                        },
+                        popEnterTransition = {
+                            slideIntoContainer(
+                                AnimatedContentScope.SlideDirection.Right,
+                                animationSpec = tween(animDuration)
+                            )
+                        },
+                        popExitTransition = {
+                            slideOutOfContainer(
+                                AnimatedContentScope.SlideDirection.Right,
+                                animationSpec = tween(animDuration)
+                            )
+                        }
+                    ) {
+                        composable(TranslateScreen.SettingScreen.route) {
+                            SettingsScreen()
+                        }
+                        composable(
+                            TranslateScreen.AboutScreen.route,
+                        ) {
+                            AboutScreen()
+                        }
+                        composable(
+                            TranslateScreen.SortResultScreen.route,
+                        ) {
+                            SortResult(Modifier.fillMaxSize())
+                        }
+                        composable(
+                            TranslateScreen.SelectLanguageScreen.route
+                        ) {
+                            SelectLanguage(modifier = Modifier.fillMaxSize())
+                        }
+                    }
+
+                    composable(TranslateScreen.PluginScreen.route) {
+                        PluginScreen()
+                    }
+                    composable(TranslateScreen.ThanksScreen.route) {
+                        ThanksScreen()
+                    }
                 }
+            }
+
+            var firstOpenApplication by rememberDataSaverState(
+                key = Consts.KEY_FIRST_OPEN_APP,
+                default = true
+            )
+            if (firstOpenApplication) {
+                AlertDialog(
+                    onDismissRequest = { },
+                    text = {
+                        MarkdownText(markdown = "请认真阅读并同意[隐私政策](https://api.funnysaltyfish.fun/trans/v1/api/privacy)后，方可使用本应用")
+                    },
+                    confirmButton = {
+                        Button(onClick = { firstOpenApplication = false }) {
+                            Text(stringResource(R.string.agree))
+                        }
+                    },
+                    dismissButton = {
+                        Button(onClick = exitAppAction) {
+                            Text(stringResource(R.string.not_agree))
+                        }
+                    }
+                )
             }
         }
     }
+
 }
 
 @Stable
