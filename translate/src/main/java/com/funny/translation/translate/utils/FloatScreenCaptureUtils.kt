be9d97ddafd21a1be7228c62bf05f6e46a1168fc
FunnySaltyFish <3036548996@qq.com>
2023-05-01 11:38:17 +0800

    屏幕翻译拖动框UI部分完成
    Former-commit-id: 3b2b96a74a2dc829ab00b761206281d1ab5211b1

--- translate/src/main/java/com/funny/translation/translate/utils/FloatScreenCaptureUtils.kt
+++ translate/src/main/java/com/funny/translation/translate/utils/FloatScreenCaptureUtils.kt
@@ -4,21 +4,32 @@ import android.util.Log
 import android.view.Gravity
 import android.view.MotionEvent
 import android.view.View
+import android.widget.ImageButton
 import android.widget.LinearLayout
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.funny.translation.helper.ScreenUtils
 import com.funny.translation.helper.VibratorUtils
 import com.funny.translation.helper.handler.runOnUI
 import com.funny.translation.translate.FunnyApplication
 import com.funny.translation.translate.R
+import com.funny.translation.translate.appCtx
 import com.lzf.easyfloat.EasyFloat
 import com.lzf.easyfloat.enums.ShowPattern
 import com.lzf.easyfloat.enums.SidePattern
+import com.smarx.notchlib.utils.ScreenUtil
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import kotlin.math.abs
 import kotlin.math.roundToInt
 
@@ -44,7 +55,10 @@ object FloatScreenCaptureUtils {
     private fun getScreenCaptureLeftTop(motionEvent: MotionEvent): Pair<Float, Float> {
         val viewLeft = motionEvent.rawX - motionEvent.x
         val viewTop = motionEvent.rawY - motionEvent.y
-        return viewLeft to viewTop
+        val statusBarHeight = EasyFloat.getFloatView(TAG_SCREEN_CAPTURE_WINDOW)?.let {
+            if (ScreenUtils.isStatusBarVisible(it)) ScreenUtil.getStatusBarHeight(appCtx) else 0
+        } ?: 0
+        return viewLeft to viewTop - statusBarHeight
     }
 
     internal fun registerDrag(
@@ -54,7 +68,7 @@ object FloatScreenCaptureUtils {
         // 1. 桌面显示悬浮球，悬浮球拖动到任意位置停两秒以上，在悬浮球左侧显示“+”图标。此时拖动悬浮球开始截屏，拖动到目的地松手后完成截屏，显示“确认”按钮，点击后保存截屏到本地，获取到Uri后开始图片翻译并显示结果。帮我实现
 
         var viewLeftTop = getScreenCaptureLeftTop(motionEvent)
-        Log.d(TAG, "viewLeftTop = $viewLeftTop")
+        // Log.d(TAG, "viewLeftTop = $viewLeftTop")
         if (!whetherInScreenCaptureMode) {
             if (plusView?.alpha == 0.5f) {
                 plusView.alpha = 1f
@@ -67,7 +81,7 @@ object FloatScreenCaptureUtils {
                     startRecordScreenJob = null
                     VibratorUtils.vibrate(100)
 
-                    runOnUI {
+                    withContext(Dispatchers.Main) {
                         // 开始截屏
                         whetherInScreenCaptureMode = true
                         // 这一段代码实际上是延迟两秒后执行的，所以要重新算一下 viewLeftTop
@@ -78,7 +92,6 @@ object FloatScreenCaptureUtils {
                             viewLeftTop.first, viewLeftTop.second
                         )
                     }
-
                 }
             }
         } else {
@@ -114,7 +127,12 @@ object FloatScreenCaptureUtils {
             EasyFloat.with(FunnyApplication.ctx)
                 .setTag(TAG_SCREEN_CAPTURE_WINDOW)
                 .setLayout(R.layout.layout_float_screen_capture) { view ->
-
+                    view.findViewById<ImageButton>(R.id.cancel_button).setOnClickListener {
+                        EasyFloat.hide(TAG_SCREEN_CAPTURE_WINDOW)
+                    }
+                    view.findViewById<ImageButton>(R.id.confirm_button).setOnClickListener {
+                        EasyFloat.hide(TAG_SCREEN_CAPTURE_WINDOW)
+                    }
                 }
                 .setDragEnable(true)
                 .setShowPattern(ShowPattern.ALL_TIME)
@@ -136,7 +154,7 @@ object FloatScreenCaptureUtils {
         Log.d(TAG, "showScreenCaptureWindow")
     }
 
-    internal fun updateScreenCaptureWindowPlace(left: Float, top: Float) {
+    private fun updateScreenCaptureWindowPlace(left: Float, top: Float) {
         // 悬浮窗的定位是以左上角为基础
         val x =
             if (left > startRecordOffset.first) startRecordOffset.first else left
@@ -162,5 +180,5 @@ object FloatScreenCaptureUtils {
 fun ScreenCaptureWindow(
 
 ) {
-    Text(text = "Hello World")
+
 }
