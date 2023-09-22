b47fc59c65363c6ac7e52cc90a01c95eda83f711
FunnySaltyFish <3036548996@qq.com>
2023-09-22 17:50:45 +0800

    输入密码时加入 * 号显示
    Former-commit-id: 73fab42dde8bb1db74888744fa94a48ce1a6b840

--- login/src/main/java/com/funny/trans/login/ui/CommonComposable.kt
+++ login/src/main/java/com/funny/trans/login/ui/CommonComposable.kt
@@ -1,5 +1,7 @@
 package com.funny.trans.login.ui
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
@@ -11,12 +13,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import com.funny.trans.login.R
 import com.funny.translation.helper.UserUtils
@@ -54,6 +60,8 @@ fun InputPassword(
     val isPwdError by remember(password) {
         derivedStateOf { password != "" && !UserUtils.isValidPassword(password) }
     }
+    // 是否不显示明文密码
+    var hiddenPassword by remember { mutableStateOf(true) }
     OutlinedTextField(modifier = Modifier.fillMaxWidth(),
         value = password,
         onValueChange = updatePassword,
@@ -62,11 +70,31 @@ fun InputPassword(
             Text(text = stringResource(R.string.password_rule))
         },
         label = { Text(text = stringResource(R.string.password)) },
+        suffix = {
+            AnimatedContent(hiddenPassword, label = "password_icon") {
+                if (it) {
+                    Icon(
+                        painterResource(id = R.drawable.ic_hidden_password),
+                        contentDescription = stringResource(R.string.click_to_show_password),
+                        modifier = Modifier.padding(end = 4.dp).clickable { hiddenPassword = false },
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+                } else {
+                    Icon(
+                        painterResource(id = R.drawable.ic_show_password),
+                        contentDescription = stringResource(R.string.click_to_hide_password),
+                        modifier = Modifier.padding(end = 4.dp).clickable { hiddenPassword = true },
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+                }
+            }
+        },
         isError = isPwdError,
         keyboardOptions = KeyboardOptions(
             keyboardType = KeyboardType.Password,
             imeAction = ImeAction.Next
-        )
+        ),
+        visualTransformation = if (hiddenPassword) PasswordVisualTransformation('*') else VisualTransformation.None
     )
 }
 
